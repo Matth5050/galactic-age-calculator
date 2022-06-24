68b996d30c252a75478d857d476663524a84cef3
Matth5050 <matthew.f.herbert@gmail.com>
2022-06-24 10:27:12 -0700

    added test to determine years left

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -5,7 +5,7 @@ describe('Calculator', () => {
   let reusablePerson;
 
   beforeEach(() => {
-    reusablePerson = new Calculator(26);
+    reusablePerson = new Calculator(26, 'male');
   });
 
   test('should return the users age in Mercury years (input / .24)', () => {
@@ -28,5 +28,11 @@ describe('Calculator', () => {
     expect(reusablePerson.jupAge).toBeCloseTo(2.19, 1);
   });
 
+  test('should return the amount of time left to live on each planet based on male gender (70.8yrs)', () => {
+    expect(reusablePerson.mercuryYearsLeft).toBeCloseTo(70.8 - reusablePerson.mercuryAge, 1);
+    expect(reusablePerson.venusYearsLeft).toBeCloseTo(70.8 - reusablePerson.venusAge, 1);
+    expect(reusablePerson.marsYearsLeft).toBeCloseTo(70.8 - reusablePerson.marsAge, 1);
+    expect(reusablePerson.jupYearsLeft).toBeCloseTo(70.8 - reusablePerson.jupAge, 1);
+  });
 
 });
