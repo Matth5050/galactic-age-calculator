1d08002508b829a5fbb55539c5fb0c182f5c0396
Matth5050 <matthew.f.herbert@gmail.com>
2022-06-24 09:24:14 -0700

    added second test with a good fail, also DRYd up code with beforeEach funciton

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,15 +1,21 @@
 import Calculator from '../src/js/calculator';
 
 describe('Calculator', () => {
+  let reusablePerson;
 
-  // beforeEach(() => {
-  //   rectangle = new Rectangle(3,5);
-  // });
+  beforeEach(() => {
+    reusablePerson = new Calculator(26);
+  });
 
   test('should return the users age in Mercury years (input/.24)', () => {
-    let testPerson = new Calculator(26)
-    testPerson.mercCalc();
-    expect(testPerson.mercuryAge).toBeCloseTo(108.33);
+    reusablePerson.mercCalc();
+    expect(reusablePerson.mercuryAge).toBeCloseTo(108.33);
   });
+
+  test('should return the users age in Venus years (input/.62)', () => {
+    
+    expect(reusablePerson.mercuryAge).toBeCloseTo(41.93);
+  });
+
 
 });
