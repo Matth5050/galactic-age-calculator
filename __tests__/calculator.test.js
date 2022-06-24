ac323e74185f83b3590d29eaee8d8cded221bd23
Matth5050 <matthew.f.herbert@gmail.com>
2022-06-24 09:51:17 -0700

    added third test for mars age and got a good fail

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -7,14 +7,19 @@ describe('Calculator', () => {
     reusablePerson = new Calculator(26);
   });
 
-  test('should return the users age in Mercury years (input/.24)', () => {
+  test('should return the users age in Mercury years (input / .24)', () => {
     reusablePerson.mercCalc();
     expect(reusablePerson.mercuryAge).toBeCloseTo(108.33);
   });
 
-  test('should return the users age in Venus years (input/.62)', () => {
+  test('should return the users age in Venus years (input / .62)', () => {
     reusablePerson.venusCalc();
     expect(reusablePerson.venusAge).toBeCloseTo(41.93,1);
+  });
+
+  test('should return the users age in Mars years (input / 1.88)', () => {
+  
+    expect(reusablePerson.marsAge).toBeCloseTo(13.8, 1);
   });
 
 
